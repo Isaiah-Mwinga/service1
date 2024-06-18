2a39b2b126b7f1d1f4fee18005d07e3255833980
Isaiah-Mwinga <motongori@marketforce360.com>
2024-06-18 12:04:20 +0300

    deploy

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7rsqcvjane6ylht-97x)rpd82!et=455wv7vkbh6=8_)kezsi8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'service1-0omv.onrender.com',
